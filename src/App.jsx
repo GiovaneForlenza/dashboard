0eea7b98cb126153ded6801ee5c4020b7256f372
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2024-05-06 19:59:41 -0300

    Installed Vite + React & Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="border">
+      <div>you don't have any quotes created yet </div>
+    </div>
+  );
+}
+
+export default App;
